d30e4a2da45da66b71299c0e4acbc80a5a1adb2b
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-05-23 22:43:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+let currentAudio = null;
+
+sounds.forEach(sound => {
+	const button = [...document.querySelectorAll(".btn")]
+		.find(btn => btn.innerText === sound);
+
+	const audio = new Audio(`.sounds/${sound}.mp3`);
+
+	button.addEventListener("click", () => {
+		if(currentAudio) {
+			currentAudio.pause();
+			currentAudio.currentTime = 0;
+		}
+
+		currentAudio = audio;
+		audio.play();
+	});
+});
+
+document.querySelector(".stop").addEventListener("click", () => {
+	if(currentAudio) {
+		currentAudio.pause();
+		currentAudio.currentTime = 0;
+	}
+});
